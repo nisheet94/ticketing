61e8f0ab7d12998537ec16ba0cfbe79a7b0d3d7a
Nisheet <nisheet94@gmail.com>
2021-07-18 03:51:51 -0400

    initial commit

--- orders/src/events/listeners/payment-created-listener.ts
+++ orders/src/events/listeners/payment-created-listener.ts
@@ -1,24 +1,30 @@
-import { Subjects, Listener, PaymentCreatedEvent } from '@nctickets51/common';
-import { Message } from 'node-nats-streaming';
-import { queueGroupName } from './queue-group-name';
-import { Order } from '../../models/order';
-
-export class PaymentCreatedListener extends Listener <PaymentCreatedEvent> {
+import {
+    Subjects,
+    Listener,
+    PaymentCreatedEvent,
+    OrderStatus,
+  } from '@nctickets51/common';
+  import { Message } from 'node-nats-streaming';
+  import { queueGroupName } from './queue-group-name';
+  import { Order } from '../../models/order';
+  
+  export class PaymentCreatedListener extends Listener<PaymentCreatedEvent> {
     subject: Subjects.PaymentCreated = Subjects.PaymentCreated;
     queueGroupName = queueGroupName;
-
-    async onMessage(data: PaymentCreatedEvent['data'], msg: Message ) {
-        const order = await Order.findById(data.orderId);
-
-        if (!order) {
-            throw new Error('Order not found');
-        }
-
-        order.set({
-            status: OrderStatus.Complete,
-        });
-        await order.save();
-
-        msg.ack();
+  
+    async onMessage(data: PaymentCreatedEvent['data'], msg: Message) {
+      const order = await Order.findById(data.orderId);
+  
+      if (!order) {
+        throw new Error('Order not found');
+      }
+  
+      order.set({
+        status: OrderStatus.Complete,
+      });
+      await order.save();
+  
+      msg.ack();
     }
-}
+  }
+  
