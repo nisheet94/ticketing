2d68ac6a35179a8673f8d927b93d4748f1763a7b
Nisheet <nisheet94@gmail.com>
2021-07-18 23:03:35 -0400

    add test script

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,13 +1,13 @@
 import Link from 'next/link';
 
-const LandingPage = ({ currentUser }) => {
+const LandingPage = ({ currentUser, tickets }) => {
   const ticketList = tickets.map((ticket) => {
     return (
       <tr key={ticket.id}>
         <td>{ticket.title}</td>
         <td>{ticket.price}</td>
         <td>
-          <Link href="/ticket/[ticketId]" as={`/tickets/${ticket.id}`}>
+          <Link href="/tickets/[ticketId]" as={`/tickets/${ticket.id}`}>
             <a>View</a>
           </Link>
         </td>
